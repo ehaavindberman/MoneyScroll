148c953ac836b3919cb5227f43bbcc52f895f442
Eric Haavind-Berman <ehaavindberman@gmail.com>
2017-06-26 16:47:35 -0400

    Update MoneyScroll.js
    update for currency and note values stored in currency table rather than switch statement, updated a bunch of other stuff too, still not at minimum viable product so I'm not afraid to just make changes now, will start commenting in the future

--- MoneyScroll.js
+++ MoneyScroll.js
@@ -5,6 +5,24 @@ var interval;
 var leftpos = [];
 var moneyPictures = [];
 var time = [];
+var currencyTable = {
+  'usd_div': {
+    'c1': {'height': 75,'width': 75,'img': 'usd/1c_usd.png','increase': .01}
+    ,'c5': {'height': 100,'width': 100,'img': 'usd/5c_usd.png','increase': .05}
+    ,'c10': {'height': 60,'width': 60,'img': 'usd/10c_usd.png','increase': .1}
+    ,'c25': {'height': 75,'width': 75,'img': 'usd/25c_usd.png','increase': .10}
+    ,'d1': {'height': 100,'width': 150,'img': 'usd/1_usd.png','increase': 1}
+  },
+  'nok_div': {
+    'k1': {'height': 75,'width': 75,'img': 'nok/1_nok.png','increase': 1}
+    ,'k5': {'height': 100,'width': 100,'img': 'nok/5_nok.png','increase': 5}
+    ,'k10': {'height': 60,'width': 60,'img': 'nok/10_nok.png','increase': 10}
+    ,'k20': {'height': 125,'width': 125,'img': 'nok/20_nok.png','increase': 20}
+    ,'k50': {'height': 150,'width': 300,'img': 'nok/50_nok.png','increase': 50}
+    ,'k100': {'height': 150,'width': 305,'img': 'nok/100_nok.png','increase': 100}
+  }
+};
+
 
 var canvas = document.getElementById('scrollerCanvas');
 var context = canvas.getContext('2d');
@@ -14,12 +32,15 @@ document.getElementById("moneyMade").innerHTML = moneyMadeDisp;
 document.getElementById("newMoney").onclick = function() {addNew()};
 document.getElementById("update").onclick = function() {updateVals()};
 
-updateVals();
+updateCurrency();
 
 // draw window
 context.beginPath();
 context.lineWidth = 1;
 context.strokeRect(0,0,canvas.width,canvas.height);
+
+updateVals();
+
 
 
 
@@ -31,103 +52,36 @@ function updateVals() {
   time = [];
 
   // get values from user input
-  var selectedCurrency = document.getElementById("currency").value;
-  salary = document.getElementById('salary').value;
+  var selectedCurrency = document.querySelector('input[name = "currency"]:checked').value;
+  var selectedNote = document.querySelector('input[name = "note"]:checked').value;
+  var salary = document.getElementById('salary').value;
   salary = salary.replace(/\,/g,'');
   salary = parseInt(salary,10);
 
   // set values based on selections
   newMoney = new Image();
-  newMoney.src = '1c_usd.png';
-  switch (selectedCurrency) {
-    case "cent":
-      dollars = 100;
-      moneyIncrease = 1;
-      moneyHeight = 75;
-      moneyWidth = 75;
-      newMoney.src = '1c_usd.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
 
-    case "dollar":
-      dollars = 1;
-      moneyIncrease = 100;
-      moneyHeight = 150;
-      moneyWidth = 330;
-      newMoney.src = '1_usd.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "5c_usd":
-      dollars = 20;
-      moneyIncrease = 5;
-      moneyHeight = 100;
-      moneyWidth = 100;
-      newMoney.src = '5c_usd.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "10c_usd":
-      dollars = 10;
-      moneyIncrease = 10;
-      moneyHeight = 60;
-      moneyWidth = 60;
-      newMoney.src = '10c_usd.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "25c_usd":
-      dollars = 4;
-      moneyIncrease = 25;
-      moneyHeight = 125;
-      moneyWidth = 125;
-      newMoney.src = '25c_usd.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "euro":
-      dollars = .89;
-      moneyIncrease = 110;
-      moneyHeight = 150;
-      moneyWidth = 300;
-      newMoney.src = '1_euro.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "loonie":
-      dollars = 1.35;
-      moneyIncrease = 75;
-      moneyHeight = 100;
-      moneyWidth = 100;
-      newMoney.src = 'loonie.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-
-    case "toonie":
-      dollars = 1.35;
-      moneyIncrease = 270;
-      moneyHeight = 100;
-      moneyWidth = 100;
-      newMoney.src = 'toonie.png';
-      moneyY = canvas.height/2 - moneyHeight/2;
-    break;
-  }
+  moneyIncrease = currencyTable[selectedCurrency][selectedNote].increase;
+  moneyHeight = currencyTable[selectedCurrency][selectedNote].height;
+  moneyWidth = currencyTable[selectedCurrency][selectedNote].width;
+  newMoney.src = currencyTable[selectedCurrency][selectedNote].img;
+  moneyY = canvas.height/2 - moneyHeight/2;
 
   var tax = document.getElementById("tax").value;
 
   var salaryTime = document.getElementById("salaryTime").value;
   switch (salaryTime) {
     case "year":
-      var divideExt = 1 / 52 /40 / 60 / 60 / 1000;
+      var timeDivider = 1 / 52 /40 / 60 / 60 / 1000;
     break;
 
     case "hour":
-      var divideExt = 1 / 60 / 60 / 1000;
+      var timeDivider = 1 / 60 / 60 / 1000;
     break;
   }
 
   // calculate speed
-  salaryPerMili = 1 / (salary*dollars*divideExt*((100-tax)/100));
+  salaryPerMili = moneyIncrease / (salary*timeDivider*((100-tax)/100));
 
   if (salaryPerMili < 500) {
     velocity = 1.84;
@@ -164,6 +118,7 @@ function updateVals() {
   }
 }
 
+
 function addNew() {
   leftpos.push(-moneyWidth);
   try  {
@@ -177,25 +132,28 @@ function addNew() {
   time.push(t);
 }
 
+
 function redraw() {
   if (leftpos[0] > canvas.width) {
     leftpos.splice(0,1);
     time.splice(0,1);
-    moneyCounter += moneyIncrease/100;
+    moneyCounter += moneyIncrease;
     var moneyMadeDisp = "Money Made: $"+moneyCounter.toFixed(2);
     document.getElementById("moneyMade").innerHTML = moneyMadeDisp;
-  } else {
+  }
+  else {
     context.clearRect(0,0,canvas.width,canvas.height);
     drawAllMoneys();
   }
 }
+
 
 function drawAllMoneys() {
   context.beginPath();
   context.lineWidth = 1;
   context.strokeRect(0,0,canvas.width,canvas.height);
 
-  for (i=0; i<leftpos.length; i++) {
+  for (var i=0; i<leftpos.length; i++) {
     context.drawImage(moneyPictures[i]
       ,leftpos[i]
       ,moneyY
@@ -205,15 +163,21 @@ function drawAllMoneys() {
   }
 }
 
+// change the currency at a button push
+function updateCurrency() {
+  var currencies = ['usd_div','nok_div']; //'mmk','try','kes'
+  for (var i=0;i<currencies.length;i++) {
+    var temp = document.getElementById(currencies[i]);
+    temp.style.display = 'none';
+  }
+  var currency_val = document.querySelector('input[name = "currency"]:checked').value;
+  var temp = document.getElementById(currency_val);
+  temp.style.display = 'inline';
+}
 
 
-
-
-
+// change background color to value selected
 function updateBackground() {
   var bg_val = document.querySelector('input[name = "bgsel"]:checked').value;
   document.getElementById("scrollerCanvas").className = bg_val;
 }
-
-
-// use a queue as the data structure
